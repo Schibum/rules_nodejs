d247bf4e6dedea45389d5b877d22ee0994217199
Manu <thembrown@gmail.com>
2020-11-25 14:59:43 +0100

    add sigterm demo

--- examples/sigterm_demo/demo.sh
+++ examples/sigterm_demo/demo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-bazel run :bin & pid=$!; sleep 1; kill -TERM $pid; wait $pid;
+launcher=`bazel run --run_under "echo " :bin`
+$launcher & pid=$!; sleep 1; kill -TERM $pid; wait $pid;
 echo "launcher script terminated"
